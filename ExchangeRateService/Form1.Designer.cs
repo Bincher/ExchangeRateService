2e2020185b4c0c6e4f12c012868ba41988cd0317
seongbin <catsb21@gmail.com>
2025-02-26 18:32:17 +0900

    초기 커밋.

---
+++ ExchangeRateService/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace ExchangeRateService
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text_today = new System.Windows.Forms.TextBox();
+            this.text_yesterday = new System.Windows.Forms.TextBox();
+            this.label_today = new System.Windows.Forms.Label();
+            this.label_yesterday = new System.Windows.Forms.Label();
+            this.text_article2 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.text_article4 = new System.Windows.Forms.TextBox();
+            this.text_article6 = new System.Windows.Forms.TextBox();
+            this.text_article5 = new System.Windows.Forms.TextBox();
+            this.text_article7 = new System.Windows.Forms.TextBox();
+            this.text_article3 = new System.Windows.Forms.TextBox();
+            this.text_article8 = new System.Windows.Forms.TextBox();
+            this.text_article9 = new System.Windows.Forms.TextBox();
+            this.text_article1 = new System.Windows.Forms.TextBox();
+            this.label_article = new System.Windows.Forms.Label();
+            this.text_article10 = new System.Windows.Forms.TextBox();
+            this.label_ai = new System.Windows.Forms.Label();
+            this.text_ai = new System.Windows.Forms.TextBox();
+            this.button_ai = new System.Windows.Forms.Button();
+            this.label_title = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // text_today
+            // 
+            this.text_today.Location = new System.Drawing.Point(108, 60);
+            this.text_today.Name = "text_today";
+            this.text_today.ReadOnly = true;
+            this.text_today.Size = new System.Drawing.Size(265, 25);
+            this.text_today.TabIndex = 0;
+            // 
+            // text_yesterday
+            // 
+            this.text_yesterday.Location = new System.Drawing.Point(108, 95);
+            this.text_yesterday.Name = "text_yesterday";
+            this.text_yesterday.ReadOnly = true;
+            this.text_yesterday.Size = new System.Drawing.Size(265, 25);
+            this.text_yesterday.TabIndex = 1;
+            // 
+            // label_today
+            // 
+            this.label_today.AutoSize = true;
+            this.label_today.Location = new System.Drawing.Point(12, 63);
+            this.label_today.Name = "label_today";
+            this.label_today.Size = new System.Drawing.Size(72, 15);
+            this.label_today.TabIndex = 2;
+            this.label_today.Text = "현재 환율";
+            this.label_today.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label_yesterday
+            // 
+            this.label_yesterday.AutoSize = true;
+            this.label_yesterday.Location = new System.Drawing.Point(12, 98);
+            this.label_yesterday.Name = "label_yesterday";
+            this.label_yesterday.Size = new System.Drawing.Size(72, 15);
+            this.label_yesterday.TabIndex = 3;
+            this.label_yesterday.Text = "어제 환율";
+            // 
+            // text_article2
+            // 
+            this.text_article2.Location = new System.Drawing.Point(12, 182);
+            this.text_article2.Name = "text_article2";
+            this.text_article2.ReadOnly = true;
+            this.text_article2.Size = new System.Drawing.Size(763, 25);
+            this.text_article2.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 213);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(760, 25);
+            this.textBox2.TabIndex = 5;
+            // 
+            // text_article4
+            // 
+            this.text_article4.Location = new System.Drawing.Point(15, 244);
+            this.text_article4.Name = "text_article4";
+            this.text_article4.ReadOnly = true;
+            this.text_article4.Size = new System.Drawing.Size(760, 25);
+            this.text_article4.TabIndex = 6;
+            // 
+            // text_article6
+            // 
+            this.text_article6.Location = new System.Drawing.Point(15, 306);
+            this.text_article6.Name = "text_article6";
+            this.text_article6.ReadOnly = true;
+            this.text_article6.Size = new System.Drawing.Size(760, 25);
+            this.text_article6.TabIndex = 7;
+            // 
+            // text_article5
+            // 
+            this.text_article5.Location = new System.Drawing.Point(15, 275);
+            this.text_article5.Name = "text_article5";
+            this.text_article5.ReadOnly = true;
+            this.text_article5.Size = new System.Drawing.Size(760, 25);
+            this.text_article5.TabIndex = 8;
+            // 
+            // text_article7
+            // 
+            this.text_article7.Location = new System.Drawing.Point(15, 337);
+            this.text_article7.Name = "text_article7";
+            this.text_article7.ReadOnly = true;
+            this.text_article7.Size = new System.Drawing.Size(760, 25);
+            this.text_article7.TabIndex = 9;
+            // 
+            // text_article3
+            // 
+            this.text_article3.Location = new System.Drawing.Point(12, 213);
+            this.text_article3.Name = "text_article3";
+            this.text_article3.ReadOnly = true;
+            this.text_article3.Size = new System.Drawing.Size(763, 25);
+            this.text_article3.TabIndex = 10;
+            // 
+            // text_article8
+            // 
+            this.text_article8.Location = new System.Drawing.Point(15, 368);
+            this.text_article8.Name = "text_article8";
+            this.text_article8.ReadOnly = true;
+            this.text_article8.Size = new System.Drawing.Size(760, 25);
+            this.text_article8.TabIndex = 11;
+            // 
+            // text_article9
+            // 
+            this.text_article9.Location = new System.Drawing.Point(15, 399);
+            this.text_article9.Name = "text_article9";
+            this.text_article9.ReadOnly = true;
+            this.text_article9.Size = new System.Drawing.Size(760, 25);
+            this.text_article9.TabIndex = 12;
+            // 
+            // text_article1
+            // 
+            this.text_article1.Location = new System.Drawing.Point(12, 151);
+            this.text_article1.Name = "text_article1";
+            this.text_article1.ReadOnly = true;
+            this.text_article1.Size = new System.Drawing.Size(763, 25);
+            this.text_article1.TabIndex = 13;
+            // 
+            // label_article
+            // 
+            this.label_article.AutoSize = true;
+            this.label_article.Location = new System.Drawing.Point(326, 133);
+            this.label_article.Name = "label_article";
+            this.label_article.Size = new System.Drawing.Size(142, 15);
+            this.label_article.TabIndex = 14;
+            this.label_article.Text = "오늘 환율 관련 뉴스";
+            this.label_article.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // text_article10
+            // 
+            this.text_article10.Location = new System.Drawing.Point(12, 430);
+            this.text_article10.Name = "text_article10";
+            this.text_article10.ReadOnly = true;
+            this.text_article10.Size = new System.Drawing.Size(763, 25);
+            this.text_article10.TabIndex = 15;
+            // 
+            // label_ai
+            // 
+            this.label_ai.AutoSize = true;
+            this.label_ai.Location = new System.Drawing.Point(411, 66);
+            this.label_ai.Name = "label_ai";
+            this.label_ai.Size = new System.Drawing.Size(54, 15);
+            this.label_ai.TabIndex = 17;
+            this.label_ai.Text = "AI 분석";
+            this.label_ai.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // text_ai
+            // 
+            this.text_ai.Location = new System.Drawing.Point(414, 95);
+            this.text_ai.Name = "text_ai";
+            this.text_ai.ReadOnly = true;
+            this.text_ai.Size = new System.Drawing.Size(361, 25);
+            this.text_ai.TabIndex = 18;
+            // 
+            // button_ai
+            // 
+            this.button_ai.Location = new System.Drawing.Point(480, 60);
+            this.button_ai.Name = "button_ai";
+            this.button_ai.Size = new System.Drawing.Size(75, 23);
+            this.button_ai.TabIndex = 19;
+            this.button_ai.Text = "분석하기";
+            this.button_ai.UseVisualStyleBackColor = true;
+            this.button_ai.Click += new System.EventHandler(this.button_ai_ClickAsync);
+            // 
+            // label_title
+            // 
+            this.label_title.AutoSize = true;
+            this.label_title.Location = new System.Drawing.Point(326, 18);
+            this.label_title.Name = "label_title";
+            this.label_title.Size = new System.Drawing.Size(122, 15);
+            this.label_title.TabIndex = 20;
+            this.label_title.Text = "환율 분석 서비스";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 462);
+            this.Controls.Add(this.label_title);
+            this.Controls.Add(this.button_ai);
+            this.Controls.Add(this.text_ai);
+            this.Controls.Add(this.label_ai);
+            this.Controls.Add(this.text_article10);
+            this.Controls.Add(this.label_article);
+            this.Controls.Add(this.text_article1);
+            this.Controls.Add(this.text_article9);
+            this.Controls.Add(this.text_article8);
+            this.Controls.Add(this.text_article3);
+            this.Controls.Add(this.text_article7);
+            this.Controls.Add(this.text_article5);
+            this.Controls.Add(this.text_article6);
+            this.Controls.Add(this.text_article4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.text_article2);
+            this.Controls.Add(this.label_yesterday);
+            this.Controls.Add(this.label_today);
+            this.Controls.Add(this.text_yesterday);
+            this.Controls.Add(this.text_today);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox text_today;
+        private System.Windows.Forms.TextBox text_yesterday;
+        private System.Windows.Forms.Label label_today;
+        private System.Windows.Forms.Label label_yesterday;
+        private System.Windows.Forms.TextBox text_article2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox text_article4;
+        private System.Windows.Forms.TextBox text_article6;
+        private System.Windows.Forms.TextBox text_article5;
+        private System.Windows.Forms.TextBox text_article7;
+        private System.Windows.Forms.TextBox text_article3;
+        private System.Windows.Forms.TextBox text_article8;
+        private System.Windows.Forms.TextBox text_article9;
+        private System.Windows.Forms.TextBox text_article1;
+        private System.Windows.Forms.Label label_article;
+        private System.Windows.Forms.TextBox text_article10;
+        private System.Windows.Forms.Label label_ai;
+        private System.Windows.Forms.TextBox text_ai;
+        private System.Windows.Forms.Button button_ai;
+        private System.Windows.Forms.Label label_title;
+    }
+}
+
---
+++ ExchangeRateService/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExchangeRateService
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadExchangeRates();
+            LoadArticles();
+            
+        }
+
+        private void LoadExchangeRates()
+        {
+            // 예시 데이터 (실제로는 API 호출로 데이터를 가져와야 함)
+            string currentRate = "1 USD = 1300 KRW";
+            string yesterdayRate = "1 USD = 1295 KRW";
+
+            // 텍스트박스에 데이터 바인딩
+            text_today.Text = $"현재 환율: {currentRate}";
+            text_yesterday.Text = $"어제 환율: {yesterdayRate}";
+        }
+
+        private void LoadArticles()
+        {
+            // 크롤링한 기사 데이터를 예시로 배열에 저장
+            string[] articles = new string[]
+            {
+                "환율 급등, 경제에 미치는 영향은?",
+                "오늘의 주요 경제 뉴스",
+                "환율 하락, 수출 기업의 반응",
+                "외환 시장의 변동성 증가",
+                "달러 강세 지속 전망",
+                "유로화 약세, 원화에 미치는 영향",
+                "금리 인상과 환율의 관계",
+                "경제 전문가들의 환율 예측",
+                "환율 변동으로 인한 투자 전략",
+                "오늘의 외환 시장 브리핑"
+            };
+
+            // 각 텍스트박스에 기사 데이터 바인딩
+            text_article1.Text = articles[0];
+            text_article2.Text = articles[1];
+            text_article3.Text = articles[2];
+            text_article4.Text = articles[3];
+            text_article5.Text = articles[4];
+            text_article6.Text = articles[5];
+            text_article7.Text = articles[6];
+            text_article8.Text = articles[7];
+            text_article9.Text = articles[8];
+            text_article10.Text = articles[9];
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button_ai_ClickAsync(object sender, EventArgs e)
+        {
+            // GPT API 호출 예제 (실제 API 키와 URL로 대체해야 함)
+            string apiKey = "api-key";
+            string prompt = "블라블라";
+
+            string analysisResult = await CallGptApi(apiKey, prompt);
+
+            // 분석 결과를 텍스트박스에 표시
+            text_ai.Text = analysisResult;
+        }
+
+        private async Task<string> CallGptApi(string apiKey, string prompt)
+        {
+            return "분석한 내용";
+        }
+    }
+}
